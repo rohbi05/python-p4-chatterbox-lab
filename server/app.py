96465a459161a4f3ffd329f72bfd417d21a62c91
shantalrohbi <shantal.rohbi@student.moringaschool.com>
2024-10-14 21:11:22 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,13 +14,40 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = Message.query.order_by(Message.updated_at)
+        response = [message.to_dict() for message in messages]
+        return make_response(response, 200)
+    elif request.method == "POST":
+        data = request.get_json() if request.is_json else request.form()
+        message = Message(**data)
+        db.session.add(message)
+        db.session.commit()
+        return make_response(message.to_dict(), 201)
 
-@app.route('/messages/<int:id>')
+
+@app.route("/messages/<int:id>", methods=["GET", "PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = db.session.get(Message, id)
+    if message is None:
+        return make_response({"error": "Message not found"}, 404)
+    if request.method == "GET":
+        return make_response(message.to_dict(), 200)
+    elif request.method == "PATCH":
+        data = request.get_json() if request.is_json else request.form()
+        for key, value in data.items():
+            setattr(message, key, value)
+        db.session.commit()
+        return make_response(message.to_dict(), 200)
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        return make_response({"message": "Message deleted"}, 200)
+    return make_response({"error": "Invalid request"}, 400)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,33 @@
+from datetime import datetime
+import pytz
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import DateTime, MetaData
+from sqlalchemy.dialects.postgresql import TIMESTAMP
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+gmt_plus_3 = pytz.timezone("Africa/Nairobi")
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
+    __tablename__ = "messages"
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(
+        DateTime(timezone=True),
+        server_default=db.func.now(),
+        default=datetime.now(gmt_plus_3),
+    )
+    updated_at = db.Column(
+        DateTime(timezone=True),
+        onupdate=datetime.now(gmt_plus_3),
+        default=datetime.now(gmt_plus_3),
+    )
